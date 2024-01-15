a8d57eabc22a211231974a5e106911f4dfb2fe21
Le_Joker_ <54770565+Titi-git@users.noreply.github.com>
2024-01-15 16:48:08 +0100

    Updated ESP32 S2 code

--- esp.cpp
+++ esp.cpp
@@ -1,39 +1,56 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <Arduino_JSON.h>
-
-const char *SSID = "Lamotte qui a la 5g";
-const char *PWD = "beex8814";
-const char* serverName = "http://192.168.172.247:5000/click";
+//#include <Arduino_JSON.h>
 
 #define A1 1
 
+WiFiClient client;
+HTTPClient http;
+
+int flag = 0;
+const char *SSID = "SSID";
+const char *PWD = "Password";
+const char* serverName = "hrrp://YourLocalIP:5000/click";
+// the setup function runs once when you press reset or power the board
 void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  //Serial.begin(9600);
   WiFi.begin(SSID, PWD);
   pinMode(A1, INPUT);
   while (WiFi.status() != WL_CONNECTED) {
     delay(100);
-    Serial.println("Connecting to WiFi..");
+    //Serial.println("Connecting to WiFi..");
   }
-  Serial.println("Connected to the WiFi network");
-  WiFiClient client;
-      HTTPClient http;
-      // Your Domain name with URL path or IP address with path
-      http.begin(client, serverName);
-      //Get
-      String serverPath = serverName ;
-      // Your Domain name with URL path or IP address with path
-      http.begin(serverPath.c_str());
+  //Serial.println("Connected to the WiFi network");
+  // Your Domain name with URL path or IP address with path
+  http.begin(client, serverName);
+  //Get
+  String serverPath = serverName ;
+  // Your Domain name with URL path or IP address with path
+  http.begin(serverPath.c_str());
 }
 
+// the loop function runs over and over again forever
 void loop() {
-    //Check WiFi connection status
-    if(A1 == HIGH){
+  //Check WiFi connection status
+  //Serial.println(analogRead(A1));
+  if(analogRead(A1) > 1000){
+    if(flag == 0){
       int httpResponseCode = http.GET();
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
+      //Serial.print("HTTP Response code: ");
+      //Serial.println(httpResponseCode);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(100);
+      digitalWrite(LED_BUILTIN, LOW);
+      flag = 1;
     }
+  }
+  else{
+    flag=0;
+  }
+
 }
+
